bdfc06587c2dfcf00988bcacaa47fc86688b9f2f
Camsz27 <camiloagr2707@gmail.com>
2021-10-24 13:54:54 -0500

    Instruments are now correctly updated

--- controllers/instrumentController.js
+++ controllers/instrumentController.js
@@ -72,6 +72,7 @@ exports.instrument_create_post = [
   },
 ];
 
+// Display form to update an instrument
 exports.instrument_update_get = function (req, res) {
   async.parallel(
     {
@@ -102,9 +103,60 @@ exports.instrument_update_get = function (req, res) {
   );
 };
 
-exports.instrument_update_post = function (req, res) {
-  res.send('Not implemented: instrument update post');
-};
+// Handle user input to update instrument
+exports.instrument_update_post = [
+  body('name', 'Invalid name').trim().isLength({ min: 1, max: 100 }).escape(),
+  body('brand', 'Invalid brand').trim().isLength({ min: 1, max: 100 }).escape(),
+  body('price', 'Invalid price')
+    .trim()
+    .isFloat({ min: 1, max: 100000 })
+    .escape(),
+  body('number_in_stock', 'Invalid stock number')
+    .trim()
+    .isInt({ min: 1, max: 1000 })
+    .escape(),
+  body('image', 'Invalid brand').trim().isLength({ min: 1, max: 100 }).escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      Category.find({}).exec(function (err, result) {
+        if (err) {
+          return next(err);
+        }
+        res.render('instrument_form', {
+          title: 'Update Instrument',
+          categories: result,
+          instrument: req.body,
+          selected_category: req.body.category,
+          errors: errors.array(),
+          but: 'Update',
+        });
+      });
+      return;
+    }
+    var instrument = new Instrument({
+      name: req.body.name,
+      brand: req.body.brand,
+      category: req.body.category,
+      price: req.body.price,
+      number_in_stock: req.body.number_in_stock,
+      image: req.body.image,
+      _id: req.params.id,
+    });
+    Instrument.findByIdAndUpdate(
+      req.params.id,
+      instrument,
+      {},
+      function (err, theInstrument) {
+        if (err) {
+          return next(err);
+        }
+        res.redirect(theInstrument.url);
+      }
+    );
+  },
+];
 
 exports.instrument_delete_get = function (req, res) {
   res.send('Not implemented: instrument delete get');
